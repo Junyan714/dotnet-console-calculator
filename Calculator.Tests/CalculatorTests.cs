9289cd9b36d7ac446ffd4397604c4a48ca00a9df
Junyan Zhang <junyan@JunyandeMacBook-Pro.local>
2024-05-24 16:23:15 -0400

    feature add function - subtract and add test function also

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -8,6 +8,11 @@ public class CalculatorTests
         Assert.Equal(6L, Add.Eval(1L, 5L));
     }
     [Fact]
+    public void TestSubtract()
+    {
+        Assert.Equal(3L, Subtract.Eval(9L, 6L));
+    }
+    [Fact]
     public void TestMult()
     {
         Assert.Equal(6L, Multiply.Eval(2L, 3L));
@@ -16,6 +21,11 @@ public class CalculatorTests
     public void TestAddOperation()
     {
         Assert.Equal(8, Evaluator.Eval("+", 6, 2));
+    }
+    [Fact]
+    public void TestSubtractOperation()
+    {
+        Assert.Equal(4, Evaluator.Eval("-", 6, 2));
     }
     [Fact]
     public void TestMultiplyOperation()
